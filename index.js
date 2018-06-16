4fec9ba9d6053860e3ca9df4be9a676ed57a9503
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-06-16 21:24:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,9 +20,8 @@ function add2(n) {
 }
 
 var funkyFunction = function() {
-   return function(){
-     var funky = "FUNKY!";
-      return funky;
+   return function() {
+      return "FUNKY!";
 }}
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
